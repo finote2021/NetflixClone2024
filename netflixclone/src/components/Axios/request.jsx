6910f233f98126fbb56bc3371ed77018e6d56c38
finote2021 <finotepawloss29@gmail.com>
2024-07-05 00:15:24 -0700

    second - comit

---
+++ netflixclone/src/components/Axios/request.jsx
@@ -0,0 +1,16 @@
+const API_KEY = "8cec7a55496c043088a9e0c9e0b0848c";
+
+const request = {
+  fetchNetflexOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default request;
+
+//axios is a functional moduel to requst api not a component
