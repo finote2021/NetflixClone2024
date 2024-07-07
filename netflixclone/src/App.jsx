48b4844fc0140d630ce9589a654d96e30635c5db
finote2021 <finotepawloss29@gmail.com>
2024-07-07 14:11:24 -0700

    file reorganized

--- netflixclone/src/App.jsx
+++ netflixclone/src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import "./App.css";
-import Row from "./components/rows/Row";
-import request from "./components/Util/requests.jsx";
-
+import Home from "./pages/Home/Home"
 
 function App() {
   return (
     <div className="App">
-      <Row title="NetflexOrginals" fetchUrl={request.fetchNetflexOrginals} />
-      <Row title="Treding Now" fetchUrl={request.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+      <Home />
     </div>
   );
 }
--- netflixclone/src/pages/Home/Home.jsx
+++ netflixclone/src/pages/Home/Home.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import Row from "../../components/rows/Row";
 import requests from "../../components/Util/requests";
-import Footer from "../../components/Footer/Footer.jsx";
 
-function Home() {
+const Home = () => {
   return (
-    <div>
+    <>
       <Header />
       <Banner />
-      <Row title="NetflexOrginals" fetchUrl={requests.fetchNetflexOrginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row
+        title="Netflix Orginals"
+        fetchUrl={requests.fetchNetflexOrginals}
+        isLargeRow={true}
+      />
+      <Row title="Trendings" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated " fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies " fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies " fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies " fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries " fetchUrl={requests.fetchDocumentaries} />
+      <br></br>
+      <br></br>
+      <br></br>
       <Footer />
-    </div>
+    </>
   );
-}
+};
 
 export default Home;
